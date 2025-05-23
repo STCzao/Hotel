4580a52aabad46a9e0af952ef386f61ab6a0e3a8
STCzao <Santicoronel0183@gmail.com>
2025-05-23 13:51:11 -0300

    Se agrego el apartado de Exclusive Offers en el Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import FeaturedDestination from '../components/FeaturedDestination/FeaturedDestination.jsx'
+import ExclusiveOffers from '../components/ExclusiveOffers/ExclusiveOffers.jsx'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
         <Hero/>
         <FeaturedDestination/>
+        <ExclusiveOffers/>
     </div>
   )
 }
