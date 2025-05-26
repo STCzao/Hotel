53cd17726cf9aacd69718c67213eec6aa631d957
STCzao <Santicoronel0183@gmail.com>
2025-05-26 10:34:33 -0300

    Error: No constate que la rama este creada y empezamos a trabajar en la page AllRooms

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
+import AllRooms from "./pages/AllRooms";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -13,6 +14,7 @@ function App() {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
       <Footer />
