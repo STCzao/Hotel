419c17867216efdf9bda4c607564e95820ae0b4b
STCzao <Santicoronel0183@gmail.com>
2025-05-30 14:16:19 -0300

    Terminamos con HotelReg y comenzamos a trabajar con el dashboard

---
+++ src/pages/Hotelowner/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../../components/Title/Title";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title
+        align="left"
+        font="outfit"
+        title="Dashboard"
+        subTitle="Monitor your room listings, track bookings and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations."
+      />
+    </div>
+  );
+};
+
+export default Dashboard;
